90b7209079ab05616960dff78cb9386b230db0da
gebeOCC <occ.gebe.barry@gmail.com>
2025-07-22 08:28:17 +0800

    change password of CBA enrolled students in summer semester

---
+++ database/seeders/ChangeSummerStudentPassword.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EnrolledStudent;
+use App\Models\SchoolYear;
+use App\Models\User;
+use App\Models\YearSection;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class ChangeSummerStudentPassword extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userIds = YearSection::where('school_year_id', 2)
+            ->join('enrolled_students', 'year_section.id', '=', 'enrolled_students.year_section_id')
+            ->join('course', 'course.id', '=', 'year_section.course_id')
+            ->where('department_id', 3)
+            ->join('users', 'users.id', '=', 'enrolled_students.student_id')
+            ->whereNotNull('users.first_login_at') // Only those who have already logged in
+            ->pluck('users.id');
+
+        User::whereIn('id', $userIds)
+            ->update([
+                'password' => Hash::make('password'),
+            ]);
+    }
+}
