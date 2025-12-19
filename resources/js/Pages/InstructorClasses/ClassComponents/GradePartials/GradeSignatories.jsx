3636c1f48c0c25dbb2256813d0b429c40c895208
gebeOCC <occ.gebe.barry@gmail.com>
2025-12-19 13:25:10 +0800

    fix grade signatories hide sometimes

--- resources/js/Pages/InstructorClasses/ClassComponents/GradePartials/GradeSignatories.jsx
+++ resources/js/Pages/InstructorClasses/ClassComponents/GradePartials/GradeSignatories.jsx
@@ -1,73 +1,71 @@
 import { Card, CardContent } from '@/Components/ui/card'
 import { usePage } from '@inertiajs/react'
-import React, { useEffect, useState } from 'react'
-import { useProgramHeadStore } from "./ProgramHeadNameSignatory";
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React from 'react'
 
 function GradeSignatories({ yearSectionSubjectsId }) {
-      const [loading, setLoading] = useState(true);
-      const { user } = usePage().props.auth;
-      const { programHead, setProgramHead } = useProgramHeadStore();
+    const { user } = usePage().props.auth
 
-      useEffect(() => {
-            if (!programHead) {
-                  axios.post(route("program-head-name", yearSectionSubjectsId))
-                        .then((res) => setProgramHead(res.data))
-                        .finally(() => setLoading(false))
-                        .catch((err) => console.error(err));
-            }
-      }, [programHead, setProgramHead, yearSectionSubjectsId]);
+    const {
+        data: programHead,
+        isLoading,
+    } = useQuery({
+        queryKey: ['program-head-name', yearSectionSubjectsId],
+        queryFn: async () => {
+            const res = await axios.post(
+                route('program-head-name', yearSectionSubjectsId)
+            )
+            return res.data
+        },
+        enabled: !!yearSectionSubjectsId,
+    })
 
-      if (loading) return <></>
+    if (isLoading || !programHead) return null
 
-      return (
-            <Card className='hidden print:block'>
-                  <CardContent className='pt-6'>
-                        <div className='flex justify-between'>
-                              <div className='text-center'>
-                                    <div className='mb-6'>
-                                          Submitted by:
-                                    </div>
-                                    <div className='w-52 border-b'>
-                                          <p>
-                                                {user.first_name.toUpperCase()}
-                                                {user.middle_name ? ` ${user.middle_name[0].toUpperCase()}. ` : ' '}
-                                                {user.last_name.toUpperCase()}
-                                          </p>
-                                    </div>
-                                    <div >
-                                          Instructor
-                                    </div>
-                              </div>
-                              <div className='text-center'>
-                                    <div className='mb-6'>
-                                          Checked by:
-                                    </div>
-                                    <div className='w-52 border-b h-6'>
-                                          <p>
-                                                {programHead.first_name.toUpperCase()}
-                                                {programHead.middle_name ? ` ${programHead.middle_name[0].toUpperCase()}. ` : ' '}
-                                                {programHead.last_name.toUpperCase()}
-                                          </p>
-                                    </div>
-                                    <div>
-                                          Program Head
-                                    </div>
-                              </div>
-                              <div className='text-center'>
-                                    <div className='mb-6'>
-                                          Acknowledged by:
-                                    </div>
-                                    <div className='w-52 border-b h-6'>
-                                          BERNADETH T. NACUA
-                                    </div>
-                                    <div>
-                                          Registrar 1
-                                    </div>
-                              </div>
+    return (
+        <Card className="hidden print:block">
+            <CardContent className="pt-6">
+                <div className="flex justify-between">
+                    <div className="text-center">
+                        <div className="mb-6">Submitted by:</div>
+                        <div className="w-52 border-b">
+                            <p>
+                                {user.first_name.toUpperCase()}
+                                {user.middle_name
+                                    ? ` ${user.middle_name[0].toUpperCase()}. `
+                                    : ' '}
+                                {user.last_name.toUpperCase()}
+                            </p>
                         </div>
-                  </CardContent>
-            </Card>
-      )
+                        <div>Instructor</div>
+                    </div>
+
+                    <div className="text-center">
+                        <div className="mb-6">Checked by:</div>
+                        <div className="w-52 border-b h-6">
+                            <p>
+                                {programHead.first_name.toUpperCase()}
+                                {programHead.middle_name
+                                    ? ` ${programHead.middle_name[0].toUpperCase()}. `
+                                    : ' '}
+                                {programHead.last_name.toUpperCase()}
+                            </p>
+                        </div>
+                        <div>Program Head</div>
+                    </div>
+
+                    <div className="text-center">
+                        <div className="mb-6">Acknowledged by:</div>
+                        <div className="w-52 border-b h-6">
+                            BERNADETH T. NACUA
+                        </div>
+                        <div>Registrar 1</div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
 }
 
 export default GradeSignatories
