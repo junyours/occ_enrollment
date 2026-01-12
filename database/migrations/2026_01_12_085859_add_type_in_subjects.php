516bb288977f89be58d991213ce7a64d05cf547a
gebeOCC <occ.gebe.barry@gmail.com>
2026-01-12 09:32:09 +0800

    add subject type

---
+++ database/migrations/2026_01_12_085859_add_type_in_subjects.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            // Add missing columns
+            $table->enum('type', ['gened'])->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // do nothing lol
+    }
+};
