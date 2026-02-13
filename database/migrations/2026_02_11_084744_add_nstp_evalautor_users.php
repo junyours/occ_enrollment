e3d6e86f165459ef23b4cc0d3d8513dc9f5b85ae
gebeOCC <occ.gebe.barry@gmail.com>
2026-02-13 14:28:36 +0800

    add nstp evaluators, make gender nullable, and add active column in users table

---
+++ database/migrations/2026_02_11_084744_add_nstp_evalautor_users.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::statement("ALTER TABLE users MODIFY user_role ENUM(
+                'faculty',
+                'student',
+                'program_head',
+                'evaluator',
+                'registrar',
+                'mis',
+                'super_admin',
+                'president',
+                'announcement_admin',
+                'guidance',
+                'librarian',
+                'ojt_coordinator',
+                'research_coordinator',
+                'vpaa',
+                'gened_coordinator',
+                'nstp_director',
+                'cwts_evaluator',
+                'rotc_evaluator',
+                'lts_evaluator') NOT NULL");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
