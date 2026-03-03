c5ae27cac74b3be44c7b97469ef029665ae37003
gebeOCC <occ.gebe.barry@gmail.com>
2026-03-03 11:46:37 +0800

    uppercase C for Components

--- resources/js/Pages/NstpDirector/Students/DownloadButton.jsx
+++ resources/js/Pages/NstpDirector/Students/DownloadButton.jsx
@@ -11,7 +11,7 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger
-} from "@/components/ui/alert-dialog";
+} from "@/Components/ui/alert-dialog";
 import { Button } from "@/Components/ui/button";
 import { useSchoolYearStore } from '@/Components/useSchoolYearStore';
 
