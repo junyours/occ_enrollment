6aaad3fbfee4b3a9ce6412411dd37f3aa3690639
gebeOCC <occ.gebe.barry@gmail.com>
2026-05-05 13:24:05 +0800

    change form 9 route to permanent record

--- routes/RegistrarRoute.php
+++ routes/RegistrarRoute.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Registrar\FormNineController;
 
 Route::middleware(['auth', 'maintenance', 'registrar'])->group(function () {
-    Route::get('/form-9', [FormNineController::class, 'view'])->name('form-9');
-    Route::get('/form-9/student/{id}', [FormNineController::class, 'studentGrades'])->name('form-9-student');
+    Route::get('/permanent-record', [FormNineController::class, 'view'])->name('permanent-record');
+    Route::get('/permanent-record/student/{id}', [FormNineController::class, 'studentGrades'])->name('permanent-record-student');
 });
