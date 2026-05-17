7aefd038b85aad9f2e56609ffd4bdc9d7922dec2
gebeOCC <occ.gebe.barry@gmail.com>
2026-05-17 14:15:06 +0800

    add billing and cashier use in MIS adding user

--- app/Http/Controllers/Mis/MisController.php
+++ app/Http/Controllers/Mis/MisController.php
@@ -113,7 +113,7 @@ class MisController extends Controller
     {
         $validated = $request->validate([
             'user_id_no' => ['required', 'unique:users,user_id_no'],
-            'user_role' => ['required', 'in:faculty,student,program_head,evaluator,registrar,president,guidance,vpaa,librarian,ojt_coordinator,research_coordinator,gened_coordinator,nstp_director'],
+            'user_role' => ['required', 'in:faculty,student,program_head,evaluator,registrar,president,guidance,vpaa,librarian,ojt_coordinator,research_coordinator,gened_coordinator,nstp_director,billing,cashier'],
             'password' => ['required', 'confirmed'],
         ], [
             'user_id_no.required' => 'User ID number is required.',
