5bde80f3d5ea8031ad6f88c3df0cc88c549443f7
gebeOCC <occ.gebe.barry@gmail.com>
2026-08-10 15:07:23 +0800

    fix header font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,8 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" /> -->
     <!-- <link href="[https://fonts.bunny.net/css?family=roboto:300,400,500,700&display=swap](https://fonts.bunny.net/css?family=roboto:300,400,500,700&display=swap)" rel="stylesheet" /> -->
-    <link href="[https://fonts.bunny.net/css?family=inter:300,400,500,600,700&display=swap](https://fonts.bunny.net/css?family=inter:300,400,500,600,700&display=swap)" rel="stylesheet" />
-
+    <link href="https://fonts.bunny.net/css?family=inter:300,400,500,600,700&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
     @routes
